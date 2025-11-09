65ff2af932fbd28637175f07f738b6e419d0b860
zarrin <zarrinrahman831@gmail.com>
2025-11-09 10:27:35 +0600

    Add files via upload

---
+++ MID/lt1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html lange="en">
+    <head>
+        <title>JS</title>
+        <head>
+            <body>
+                <h1>This is js lab</h1>
+                <script>
+                    document.write("Hello World");
+                    console.log("Hello i am the console");
+                    //alert("hello");
+                    //variable
+                    var name = "John";
+                    let age = 25;
+                    
+    
